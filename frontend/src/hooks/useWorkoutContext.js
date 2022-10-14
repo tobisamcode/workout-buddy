87f7e8d8eaf4c6d700be3f013822429e9634869f
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-14 19:58:46 +0100

    using the useContext hooks and the react context to manage the global state of the app

---
+++ frontend/src/hooks/useWorkoutContext.js
@@ -0,0 +1,14 @@
+import { WorkoutsContext } from "../context/workoutContext";
+import { useContext } from "react";
+
+export const useWorkoutContext = () => {
+  const context = useContext(WorkoutsContext);
+
+  if (!context) {
+    throw Error(
+      "useWorkoutsContext must be used inside a WorkoutsContextProvider"
+    );
+  }
+
+  return context;
+};

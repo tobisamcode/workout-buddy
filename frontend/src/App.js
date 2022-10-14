0cdfa91bd5bbab7bc918eb7d868c5967985dd845
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-14 14:46:59 +0100

    creating pages and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/navbar.component";
 import Home from "./pages/home/home.components";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />

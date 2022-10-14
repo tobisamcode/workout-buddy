5f5f254cc0e5a5ea1c2e5bf898611f9fa41e462c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-14 14:47:16 +0100

    creating pages and components

--- frontend/src/components/navbar/navbar.component.jsx
+++ frontend/src/components/navbar/navbar.component.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./navbar.styles.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <header>
+        <div className="container">
+          <Link to="/">
+            <h1>Workout Buddy</h1>
+          </Link>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Navbar;

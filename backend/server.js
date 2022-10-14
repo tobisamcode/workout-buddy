5a9419e308659d4fb04c49a6d3d18f8eb999c054
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-14 01:27:13 +0100

    IMPLEMENTING THE ROUTES

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const bodyParser = require("body-parser");
 
 const workoutRoutes = require("./routes/workouts");
 
@@ -10,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT;
 
 // Middleware
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);

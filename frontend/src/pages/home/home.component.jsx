8ba40e34fdb4316c7a45e1e5477eb3a4584654eb
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-14 17:44:55 +0100

    creating pages and components

---
+++ frontend/src/pages/home/home.component.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      console.log(response);
+
+      const data = await response.json();
+      console.log(data);
+
+      if (response.ok) {
+        setWorkouts(data);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;

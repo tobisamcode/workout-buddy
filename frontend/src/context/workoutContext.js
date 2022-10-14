cadb7f03ab7e8f97ef2e52cfcea97818cebf1da7
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-14 19:58:43 +0100

    using the useContext hooks and the react context to manage the global state of the app

--- frontend/src/context/workoutContext.js
+++ frontend/src/context/workoutContext.js
@@ -1,10 +1,29 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const WorkoutsContext = createContext();
 
+export const workoutReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts]
+      };
+    default:
+      return state;
+  }
+};
+
 export const WorkoutContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutReducer, {
+    workouts: null
+  });
+
   return (
-    <WorkoutsContext.Provider>
+    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
       {children}
     </WorkoutsContext.Provider>
   );

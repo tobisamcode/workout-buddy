946d8481658688834cdade057173f339dd63c1ec
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-14 01:18:14 +0100

    IMPLEMENTING THE ROUTES

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 const express = require("express");
 require("dotenv").config();
 
+const workoutRoutes = require("./routes/workouts");
+
 // express app
 
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome home" });
+// Middleware
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
+
+// - Routes
+
+app.use("/api/workouts", workoutRoutes);
 
 // listen for request
 app.listen(PORT, () => {
-  console.log("serving is running on port: 4000");
+  console.log("serving is running on port:", PORT);
 });

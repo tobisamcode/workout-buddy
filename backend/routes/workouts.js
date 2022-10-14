69d598d7395e161addbd0673c609e8ec9629dd72
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-14 01:18:18 +0100

    IMPLEMENTING THE ROUTES

---
+++ backend/routes/workouts.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const workoutRouter = express.Router();
+
+// GET all workouts
+workoutRouter.get("/", (req, res) => {
+  res.json({ message: "GET all workouts" });
+});
+
+// GET a single workout
+workoutRouter.get("/:id", (req, res) => {
+  res.json({ message: "GET a single workout" });
+});
+
+// POST a new workout
+workoutRouter.post("/", (req, res) => {
+  res.json({ message: "POST a new workout" });
+});
+
+// DELETE a  workout
+workoutRouter.get("/:id", (req, res) => {
+  res.json({ message: "DELETE a workout" });
+});
+
+// UPDATE a  workout
+workoutRouter.get("/", (req, res) => {
+  res.json({ message: "UPDATE a workout" });
+});
+
+module.exports = workoutRouter;

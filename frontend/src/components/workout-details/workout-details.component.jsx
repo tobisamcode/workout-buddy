88844b2f3ddcd7ef65e5de62d290bfea7320fe87
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-14 18:48:32 +0100

    posting the api/server via the client

---
+++ frontend/src/components/workout-details/workout-details.component.jsx
@@ -0,0 +1,22 @@
+import "./workout-details.styles.scss";
+
+import React from "react";
+
+const WorkoutDetails = ({ workout }) => {
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Loads (kg):</strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps:</strong>
+        {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+};
+
+export default WorkoutDetails;

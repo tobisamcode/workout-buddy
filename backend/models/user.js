af3398f0ab1eace5815cef151e10a049110473ad
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-17 00:57:52 +0100

    implementing the authentication login and sign up

---
+++ backend/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;

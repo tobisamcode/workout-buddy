0a575d4566eb1e673bdb1d0d0c1f258bd251380f
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-17 02:36:05 +0100

    implementing the authentication login and sign up using the the json web token:jwt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,7 +6,20 @@ const createToken = _id => {
 };
 
 // login user
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    // create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // signup user
 const signupUser = async (req, res) => {

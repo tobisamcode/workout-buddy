551ecec66ebcb8dce16a36c8bcb03dcb2971466a
tobisamcode <samueloluwatobiloba48@gmail.com>
2023-01-05 15:11:37 +0100

    implementing auth

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -15,7 +15,7 @@ const loginUser = async (req, res) => {
     // create token
     const token = createToken(user._id);
 
-    res.status(200).json({ email, token });
+    return res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }

3cabdc0d1d0308959c8ef77d680b26b743fa6f33
tobisamcode <samueloluwatobiloba48@gmail.com>
2023-01-06 00:06:53 +0100

    hooks in authentication

--- frontend/src/components/navbar/navbar.components.jsx
+++ frontend/src/components/navbar/navbar.components.jsx
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../../hooks/useLogout";
 
 import "./navbar.styles.scss";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <header>
       <div className="container">
         <Link to="/">
           <h1>Workout Buddy</h1>
         </Link>
+
+        <nav>
+          <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>
       </div>
     </header>
   );
